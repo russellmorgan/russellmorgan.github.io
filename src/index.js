b273aa0d17e9820b07966e5204da1bad76bd50a6
Russell Morgan <russell.morgan@contractors.lesschwab.com>
2018-09-01 15:26:08 -0400

    user interface slides move down

--- src/index.js
+++ src/index.js
@@ -1,22 +1,57 @@
 import anime from 'animejs'
 
-let moveCircle = () => {
-  anime({
-    targets: '.more',
-    translateY: [
-      { value: '-100vh', duration: 1000 }
-    ],
-    // rotate: '1turn',
-    // backgroundColor: '#FFF',
-    duration: 2000,
-    loop: 1,
+let currentSlide;
+
+// Hide intro
+
+let hideIntro = () => {
+  currentSlide = '.details';
+  let showDetails = anime.timeline();
+  showDetails
+  .add({
+    targets: '.intro',
+    opacity: 0,
+    duration: 500,
+    easing: 'linear'
+  })
+  .add({
+    targets: '.details',
+    translateY: '-100vh',
+    duration: 1000,
     easing: 'easeOutElastic',
-    elasticity: 400,
-    complete: () => {
-      console.log('anim complete');
-    }
+    offset: 1000
+  })
+}
+
+
+// Swap slides
+let swapSlides = (slide) => {
+  let showHideSlide = anime.timeline();
+  showHideSlide
+  .add({
+    targets: currentSlide,
+    translateY: '100vh',
+    duration: 1000,
+    easing: 'easeOutQuad'
+  })
+  .add({
+    targets: slide,
+    translateY: '-200vh',
+    duration: 1000,
+    easing: 'easeOutElastic'
   });
 }
 
-let btn = document.querySelector('.next-section');
-btn.addEventListener('click', moveCircle, false);
+
+// Navigation binding
+const showMore = document.querySelector('.intro__more');
+showMore.addEventListener('click', hideIntro, false); 
+
+const showDetailsLinks = document.querySelectorAll('.identifier__details');
+const showProjectsLinks = document.querySelectorAll('.identifier__projects');
+const showContactLinks = document.querySelectorAll('.identifier__contact');
+
+showDetailsLinks.forEach( (el) => { el.addEventListener('click', () => { swapSlides('.details') }, false); }) 
+showProjectsLinks.forEach( (el) => { el.addEventListener('click', () => { swapSlides('.projects') } , false); }) 
+showContactLinks.forEach( (el) => { el.addEventListener('click', () => { swapSlides('.contact') }, false); }) 
+
